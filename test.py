230e3620f870cc7d1be1efddd262c61ff12d2b31
Abdulrahman Abdo <aboodabdo347@gmail.com>
2026-08-25 01:20:38 +0300

    now displays userId

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from urllib.request import urlopen, Request
 from urllib.error import HTTPError, URLError
-
+import json
 api_link = "https://api.gametools.network/bf1/stats/?name={player_id}&platform=pc"
 user_input = input("Enter the player ID: ")
 api_link = api_link.format(player_id=user_input)
@@ -11,7 +11,7 @@ try:
     with urlopen(request) as response:
         if response.status == 200:
             data = response.read().decode("utf-8")
-            print(data)
+            data = json.loads(data)
+            print(data["userId"])
 except (HTTPError, URLError) as error:
     print(f"Request failed: {error}")
-    
